ed825fbbc43c7a750f6830eaf51562ddc9dd2426
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-20 21:34:45 +0530

    added view

---
+++ View.java
@@ -0,0 +1,25 @@
+class View{
+		static String []fat={"ashdh","dsdadfa","dafaef","sdda"};
+		static String dog="wddadeadaede";
+	public static void main(String[] args){
+		char[] gun={'*','6',';','7','!'};
+		System.out.println();
+		System.out.println(gun[2]);
+		
+		String iub="dwadadfafrfrgr";
+		System.out.println(iub);
+		System.out.println();
+		
+		System.out.println(eat[2]);
+
+		System.out.println(fat[3]);
+		System.out.println(dog);
+		System.out.println(fat[3]);
+		System.out.println(toy);
+		float dig=8064/67f;
+		System.out.println(dig);
+	}
+		static int toy=23456/876;
+		static byte []eat={32,44,55,123};
+		
+}
